abb79d17feae9d211d779b2289fb0061c07bb9d0
R Tushar <dmrtushar@gmail.com>
2021-05-18 22:36:57 +0530

    fourth commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@
     let back = document.querySelector(".btn-back");
     let sin = document.querySelector(".btn-sin");
     let cos = document.querySelector(".btn-cos");
+    let tan = document.querySelector(".btn-tan");
+    let square = document.querySelector(".btn-square");
+    let lan = document.querySelector(".btn-lan");
+    let pie = document.querySelector(".btn-pie");
 
 
     buttons.forEach(function(button){
@@ -33,13 +37,35 @@
     })
 
     sin.addEventListener("click",function(e){
-       let answer = Math.sin(screen.value);
+        screen.value = screen.value * 0.0174533;
+        let answer = Math.sin(screen.value);
         screen.value = answer
     })
 
     cos.addEventListener("click",function(e){
+        screen.value = screen.value * 0.0174533;
         let answer = Math.cos(screen.value);
         screen.value = answer
+    })
+    tan.addEventListener("click",function(e){
+        screen.value = screen.value * 0.0174533;
+        let answer = Math.tan(screen.value);
+        screen.value = answer
+    })
+    square.addEventListener("click",function(e){
+        let answer = screen.value*screen.value;
+        screen.value = answer
+    })
+    lan.addEventListener("click",function(e){
+        let answer = Math.log(screen.value);
+        screen.value = answer
+    })
+    pie.addEventListener("click",function(e){
+       if(screen.value === ""){
+           screen.value =  3.1415926536 ;
+       }else{
+           screen.value = screen.value * 3.1415926536
+       }
     })
 
 })();
