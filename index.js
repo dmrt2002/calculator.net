468745535bcbaa3721372188a18a67e96b025c7b
dmrt2002 <72452238+dmrt2002@users.noreply.github.com>
2021-05-19 17:55:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,23 @@
     let per = document.querySelector(".btn-per");
     let rec = document.querySelector(".btn-rec");
     let fac = document.querySelector(".btn-fact");
+    var errName = document.querySelector(".error"); 
 
 
     buttons.forEach(function(button){
         button.addEventListener("click",function(e){
             let value = e.target.dataset.num;
             screen.value += value;
+            errName.innerHTML += value
         })
     });
 
     equal.addEventListener("click",function(e){
         if(screen.value === ""){
-            screen.value = "Enter a Value";
+            errName.innerHTML = "Enter a value"
+            setTimeout(function(){
+                errName.innerHTML = ""
+              },5000);
         }else{
             let answer =eval(screen.value);
             screen.value = answer;
@@ -36,33 +41,40 @@
 
     clear.addEventListener("click",function(e){
         screen.value = "";
+        errName.innerHTML = "";
     })
 
     back.addEventListener("click",function(e){
         screen.value = screen.value.substring(0,screen.value.length -1);
+        errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -1);
     })
 
     sin.addEventListener("click",function(e){
+        errName.innerHTML = "sin " + screen.value
         screen.value *= 0.0174533;
         let answer = Math.sin(screen.value);
         screen.value = answer
     })
 
     cos.addEventListener("click",function(e){
-        screen.value = screen.value * 0.0174533;
+        errName.innerHTML = "cos " + screen.value
+        screen.value *= 0.0174533;
         let answer = Math.cos(screen.value);
         screen.value = answer
     })
     tan.addEventListener("click",function(e){
+        errName.innerHTML = "tan " + screen.value
         screen.value = screen.value * 0.0174533;
         let answer = Math.tan(screen.value);
         screen.value = answer
     })
     square.addEventListener("click",function(e){
+        errName.innerHTML = screen.value^2
         let answer = screen.value*screen.value;
         screen.value = answer
     })
     lan.addEventListener("click",function(e){
+        errName.innerHTML = "log " + screen.value
         let answer = Math.log(screen.value);
         screen.value = answer
     })
@@ -70,34 +82,47 @@
        if(screen.value === ""){
            screen.value =  3.1415926536 ;
        }else{
-           screen.value *= 3.1415926536
+           errName.innerHTML = screen.value + " * π"
+           screen.value *= 3.1415926536;
        }
     })
 
     sqrt.addEventListener("click",function(e){
+        errName.innerHTML = "√" + screen.value
         let answer = Math.sqrt(screen.value);
         screen.value = answer
     })
 
     e.addEventListener("click",function(e){
-        screen.value *=  2.7182818285
+        errName.innerHTML =  + screen.value +"*e"
+        screen.value *= 2.7182818285
     })
 
     per.addEventListener("click",function(e){
-        screen.value *= 0.01
+        if (screen.value === 0){
+            errName.innerHTML = "Enter a Value"
+            setTimeout(function(){
+                errName.innerHTML = ""
+              },5000);
+        } else {
+            errName.innerHTML = screen.value + "%"
+            screen.value *= 0.01
+        }
     })
 
     cube.addEventListener("click",function(e){
+        errName.innerHTML = screen.value^3 
         screen.value = screen.value * screen.value * screen.value ;
     })
 
     rec.addEventListener("click",function(e){
+        errName.innerHTML = "1/"+ screen.value
         screen.value = 1/screen.value 
     })
 
     fac.addEventListener("click",function(e){
         if(screen.value<0){
-            screen.value = "Error"
+            errName.innerHTML = "Error"
         }
         else if (screen.value === 0){
             screen.value = "1"
@@ -108,6 +133,7 @@
                 fact *= i ;
             }
             screen.value = fact
+            errName.innerHTML = screen.value + "!"
         }
     })
 
