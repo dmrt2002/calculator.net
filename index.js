d6b22bf3c94f01e20e1b115aed6897b1eb629d02
R Tushar <dmrtushar@gmail.com>
2021-05-19 14:55:22 +0530

    fourth commit

--- index.js
+++ index.js
@@ -10,6 +10,12 @@
     let square = document.querySelector(".btn-square");
     let lan = document.querySelector(".btn-lan");
     let pie = document.querySelector(".btn-pie");
+    let sqrt = document.querySelector(".btn-root");
+    let cube = document.querySelector(".btn-cube");
+    let e = document.querySelector(".btn-e");
+    let per = document.querySelector(".btn-per");
+    let rec = document.querySelector(".btn-rec");
+    let fac = document.querySelector(".btn-fact");
 
 
     buttons.forEach(function(button){
@@ -37,7 +43,7 @@
     })
 
     sin.addEventListener("click",function(e){
-        screen.value = screen.value * 0.0174533;
+        screen.value *= 0.0174533;
         let answer = Math.sin(screen.value);
         screen.value = answer
     })
@@ -64,8 +70,45 @@
        if(screen.value === ""){
            screen.value =  3.1415926536 ;
        }else{
-           screen.value = screen.value * 3.1415926536
+           screen.value *= 3.1415926536
        }
+    })
+
+    sqrt.addEventListener("click",function(e){
+        let answer = Math.sqrt(screen.value);
+        screen.value = answer
+    })
+
+    e.addEventListener("click",function(e){
+        screen.value *=  2.7182818285
+    })
+
+    per.addEventListener("click",function(e){
+        screen.value *= 0.01
+    })
+
+    cube.addEventListener("click",function(e){
+        screen.value = screen.value * screen.value * screen.value ;
+    })
+
+    rec.addEventListener("click",function(e){
+        screen.value = 1/screen.value 
+    })
+
+    fac.addEventListener("click",function(e){
+        if(screen.value<0){
+            screen.value = "Error"
+        }
+        else if (screen.value === 0){
+            screen.value = "1"
+        }
+        else {
+            let fact = 1;
+            for (i = 1; i <= screen.value ; i++){
+                fact *= i ;
+            }
+            screen.value = fact
+        }
     })
 
 })();
