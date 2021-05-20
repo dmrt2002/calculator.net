dab8c4beadc73620408bc863f6abb1eec398620f
dmrt2002 <72452238+dmrt2002@users.noreply.github.com>
2021-05-20 19:27:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,32 +16,52 @@
     let per = document.querySelector(".btn-per");
     let rec = document.querySelector(".btn-rec");
     let fac = document.querySelector(".btn-fact");
-    var errName = document.querySelector(".error"); 
+    var errName = document.querySelector(".error");
+    var clearhist = document.querySelector(".btn-clearhistory");
+    var Error = document.querySelector(".error1");
+
 
 
     buttons.forEach(function(button){
         button.addEventListener("click",function(e){
+            if(screen.value === "/" || screen.value === "+" || screen.value === "-" || screen.value === "*" ){
+                Error.innerHTML = "Enter a Value" ;
+                setTimeout(function(){
+                    Error.innerHTML = ""
+                  },3000);
+                  errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+              screen.value = ""
+            } else {
             let value = e.target.dataset.num;
             screen.value += value;
             errName.innerHTML += value
+            }
         })
     });
 
     equal.addEventListener("click",function(e){
         if(screen.value === ""){
-            errName.innerHTML = "Enter a value"
+            Error.innerHTML = "Enter a value"
             setTimeout(function(){
-                errName.innerHTML = ""
-              },5000);
+                Error.innerHTML = ""
+              },3000);
         }else{
             let answer =eval(screen.value);
             screen.value = answer;
+            errName.innerHTML += "=" + answer;
         }
     })
 
     clear.addEventListener("click",function(e){
+        if(screen.value === ""){
+            errName.innerHTML = ""
+        }else {
+            errName.innerHTML += '<br/>';
+        }
         screen.value = "";
-        errName.innerHTML = "";
     })
 
     back.addEventListener("click",function(e){
@@ -50,74 +70,261 @@
     })
 
     sin.addEventListener("click",function(e){
-        errName.innerHTML = "sin " + screen.value
-        screen.value *= 0.0174533;
-        let answer = Math.sin(screen.value);
-        screen.value = answer
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+                     errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+                     errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                     errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") )  {
+                         errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                         errName.innerHTML = errName.innerHTML + "sin " + screen.value
+
+                    }else{
+                           errName.innerHTML = "sin" + " " + screen.value
+                        }
+             screen.value *= 0.0174533;
+             let answer = Math.sin(screen.value);
+             screen.value = answer
+             errName.innerHTML += "= " + answer;
     })
 
     cos.addEventListener("click",function(e){
-        errName.innerHTML = "cos " + screen.value
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("tan") || errName.innerHTML.includes("cos") ||
+                     errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+                     errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                     errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+                     errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                     errName.innerHTML = errName.innerHTML + "cos " + screen.value
+        }else{
+            errName.innerHTML = "cos" + " " + screen.value
+        }
         screen.value *= 0.0174533;
         let answer = Math.cos(screen.value);
         screen.value = answer
+        errName.innerHTML += "= " + answer;
     })
     tan.addEventListener("click",function(e){
-        errName.innerHTML = "tan " + screen.value
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+              errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+              errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+              errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("!") ){
+              errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+              errName.innerHTML = errName.innerHTML + "tan " + screen.value
+        }else{
+            errName.innerHTML = "tan" + " " + screen.value
+        }
         screen.value = screen.value * 0.0174533;
         let answer = Math.tan(screen.value);
         screen.value = answer
+        errName.innerHTML += "= " + answer;
     })
     square.addEventListener("click",function(e){
-        errName.innerHTML = screen.value^2
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        } else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+                     errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+                     errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                     errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("!") ){
+                     errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                     errName.innerHTML = errName.innerHTML + screen.value + "^2";
+        }else{
+            errName.innerHTML = screen.value + "^2" ;
+        }
         let answer = screen.value*screen.value;
         screen.value = answer
+        errName.innerHTML += "= " + answer;
     })
     lan.addEventListener("click",function(e){
-        errName.innerHTML = "log " + screen.value
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("log") || errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+                     errName.innerHTML.includes("^2") || errName.innerHTML.includes("π") || errName.innerHTML.includes("log") ||
+                     errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                     errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("!")) {        
+                     errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                     errName.innerHTML = errName.innerHTML +"log " + screen.value
+        }else{
+            errName.innerHTML = "log " + screen.value
+        }
         let answer = Math.log(screen.value);
         screen.value = answer
+        errName.innerHTML += "= " + answer;
     })
     pie.addEventListener("click",function(e){
-       if(screen.value === ""){
-           screen.value =  3.1415926536 ;
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        } else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+                errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+                errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("!")){
+                errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                errName.innerHTML = errName.innerHTML + screen.value + " * π"
        }else{
            errName.innerHTML = screen.value + " * π"
-           screen.value *= 3.1415926536;
        }
+       screen.value *= 3.1415926536;
+       errName.innerHTML += "= " + screen.value;
     })
 
     sqrt.addEventListener("click",function(e){
-        errName.innerHTML = "√" + screen.value
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+                     errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+                     errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+                     errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+                     errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                     errName.innerHTML = errName.innerHTML + "√" + screen.value
+        }else{
+            errName.innerHTML = "√" + screen.value
+        }
         let answer = Math.sqrt(screen.value);
         screen.value = answer
+        errName.innerHTML += "= " + answer;
     })
 
     e.addEventListener("click",function(e){
-        errName.innerHTML =  + screen.value +"*e"
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        } else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+        errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+        errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+        errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+            errName.innerHTML = errName.innerHTML + "e *" + screen.value
+        }else{
+            errName.innerHTML = "e *" + screen.value
+        }
         screen.value *= 2.7182818285
+        errName.innerHTML += "= " + screen.value;
     })
 
     per.addEventListener("click",function(e){
-        if (screen.value === 0){
-            errName.innerHTML = "Enter a Value"
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
             setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
                 errName.innerHTML = ""
-              },5000);
+              },1000);
+        } 
+      if (errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+        errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+        errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+        errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+            errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
         } else {
             errName.innerHTML = screen.value + "%"
             screen.value *= 0.01
+            errName.innerHTML += "= " + screen.value;
         }
     })
 
     cube.addEventListener("click",function(e){
-        errName.innerHTML = screen.value^3 
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        } else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+        errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+        errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+        errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+            errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+            errName.innerHTML = errName.innerHTML + screen.value^3 
+        }else{
+            errName.innerHTML = screen.value^3 
+        }
         screen.value = screen.value * screen.value * screen.value ;
+        errName.innerHTML += "= " + screen.value;
     })
 
     rec.addEventListener("click",function(e){
-        errName.innerHTML = "1/"+ screen.value
+        if(screen.value === ""){
+            Error.innerHTML = "Enter a value"
+            setTimeout(function(){
+                Error.innerHTML = ""
+              },3000);
+              errName.innerHTML = ""
+              setTimeout(function(){
+                errName.innerHTML = ""
+              },1000);
+        }else if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+        errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+        errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+        errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+            errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+            errName.innerHTML = errName.innerHTML + "1/" + screen.value
+        }else{
+            errName.innerHTML = "1/"+ screen.value
+        }
         screen.value = 1/screen.value 
+        errName.innerHTML += "= " + screen.value;
     })
 
     fac.addEventListener("click",function(e){
@@ -132,9 +339,22 @@
             for (i = 1; i <= screen.value ; i++){
                 fact *= i ;
             }
+            if(errName.innerHTML.includes("sin") || errName.innerHTML.includes("cos") || errName.innerHTML.includes("tan") ||
+            errName.innerHTML.includes("^2") || errName.innerHTML.includes("log") || errName.innerHTML.includes("π") || 
+            errName.innerHTML.includes("√") || errName.innerHTML.includes("e") || errName.innerHTML.includes("%") || 
+            errName.innerHTML.includes("^3") || errName.innerHTML.includes("1/") || errName.innerHTML.includes("1/") ){
+                errName.innerHTML = errName.innerHTML.substring (0, errName.innerHTML.length -screen.value.length);
+                errName.innerHTML = errName.innerHTML + screen.value + "!"
+            }else{
+                errName.innerHTML = screen.value + "!"
+            }
             screen.value = fact
-            errName.innerHTML = screen.value + "!"
+            errName.innerHTML += "= " + screen.value;
         }
+    })
+
+    clearhist.addEventListener("click",function (e){
+      errName.innerHTML = "";
     })
 
 })();
